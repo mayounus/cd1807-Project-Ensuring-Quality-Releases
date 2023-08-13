5b665ea5268d7fde78b88896392e69ebe9571e5e
Mohammad Younus <mayounus@gmail.com>
2023-08-13 15:19:14 -0400

    Updated yaml

--- terraform/modules/appservice/appservice.tf
+++ terraform/modules/appservice/appservice.tf
@@ -2,7 +2,7 @@ resource "azurerm_service_plan" "test" {
   name                = "${var.application_type}-${var.resource_type}"
   location            = "${var.location}"
   resource_group_name = "${var.resource_group}"
-  os_type             = "Linux"
+  os_type             = "Windows"
   sku_name            = "F1"
 }
 
@@ -14,12 +14,12 @@ resource "azurerm_linux_web_app" "test" {
 
   app_settings = {
     "WEBSITE_RUN_FROM_PACKAGE" = 0
-    SCM_DO_BUILD_DURING_DEPLOYMENT = true
+    #SCM_DO_BUILD_DURING_DEPLOYMENT = true
   }
   site_config {
     always_on = false
-    application_stack {
-       dotnet_version = "6.0"
+    #application_stack {
+       #dotnet_version = "6.0"
     }
   }
 }
